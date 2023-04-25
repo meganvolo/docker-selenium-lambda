7d06702e4ff87789a438b37f12df0c6cf2fb1bc1
meganvolo <115667745+meganvolo@users.noreply.github.com>
2023-04-25 13:12:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,39 +27,37 @@ def handler(event=None, context=None):
                               options=options)
     driver.get(booking_site_url)
     
-    try:
-        driver.get(booking_site_url)
-        driver.find_element(By.CSS_SELECTOR, '.sarsa-day-picker-range-controller-month-navigation-button.right').click() 
-        sleep(.5)
-        calendar_days = driver.find_elements(By.CLASS_NAME, 'CalendarDay')
-        for day in calendar_days: 
-            if(day.get_attribute('aria-label') == day_of_week):    
-                day.click()
-        times_block = driver.find_elements(By.CLASS_NAME, 'venue-reservation-radio-pill')
-        for time_block in times_block:
-            times = time_block.find_elements(By.XPATH, './div')[2].find_elements(By.XPATH, './div/div/label')
-            for time in times:
-                try:
-                    timeInput = time.find_element(By.XPATH, './/input')
-                    value = timeInput.get_attribute('value')
-                    if(value == res_time):
-                        time.click()            
-                except Exception as e:
-                    continue  	
-        driver.find_element(By.ID, 'group_size').send_keys('50')
-        button = driver.find_element(By.CSS_SELECTOR, '.sarsa-button.sarsa-button-primary')
-        button.click()
-        sleep(.5)
-        driver.find_element(By.ID, 'email').send_keys(login_email)
-        driver.find_element(By.ID, 'rec-acct-sign-in-password').send_keys(login_password)
-        buttons = driver.find_elements(By.CSS_SELECTOR, '.sarsa-button.sarsa-button-primary')
-        for button in buttons: 
-            if(button.get_attribute('aria-label') == 'Log In'):
-                button.click()
-                break  
-        sleep(1)
+    driver.get(booking_site_url)
+    driver.find_element(By.CSS_SELECTOR, '.sarsa-day-picker-range-controller-month-navigation-button.right').click() 
+    sleep(.5)
+    calendar_days = driver.find_elements(By.CLASS_NAME, 'CalendarDay')
+    for day in calendar_days: 
+        if(day.get_attribute('aria-label') == day_of_week):    
+            day.click()
+    times_block = driver.find_elements(By.CLASS_NAME, 'venue-reservation-radio-pill')
+    for time_block in times_block:
+        times = time_block.find_elements(By.XPATH, './div')[2].find_elements(By.XPATH, './div/div/label')
+        for time in times:
+            try:
+                timeInput = time.find_element(By.XPATH, './/input')
+                value = timeInput.get_attribute('value')
+	        if(value == res_time):
+                   time.click()            
+            except Exception as e:
+                continue  	
+    driver.find_element(By.ID, 'group_size').send_keys('50')
+    button = driver.find_element(By.CSS_SELECTOR, '.sarsa-button.sarsa-button-primary')
+    button.click()
+    sleep(.5)
+    driver.find_element(By.ID, 'email').send_keys(login_email)
+    driver.find_element(By.ID, 'rec-acct-sign-in-password').send_keys(login_password)
+    buttons = driver.find_elements(By.CSS_SELECTOR, '.sarsa-button.sarsa-button-primary')
+    for button in buttons: 
+        if(button.get_attribute('aria-label') == 'Log In')
+	    button.click()
+            break  
+    sleep(1)
 	
-        driver.get("https://example.com/")
-        return driver.find_element(by=By.XPATH, value="//html").text
-    except Exception as e:
-        print(e)
+    driver.get("https://example.com/")
+    sleep(1)
+    return driver.find_element(by=By.XPATH, value="//html").text
