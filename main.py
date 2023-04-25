8bef5cc78d8c3ec5c9bd30f8ac9366b4199cf4b7
Megan Gerstbauer <megan.gerstbauer@gmail.com>
2023-04-25 14:18:52 -0400

    update

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def handler(event=None, context=None):
     '''
         Login to account
         '''
-    driver.find_element(By.ID, 'email').send_keys(login_email)
+    driver.find_element(By.ID, 'email').send_keys(event["login_email"])
     driver.find_element(By.ID, 'rec-acct-sign-in-password').send_keys(login_password)
     buttons = driver.find_elements(By.CSS_SELECTOR, '.sarsa-button.sarsa-button-primary')
     for button in buttons: 
